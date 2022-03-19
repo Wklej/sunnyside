d15cc2d639ebe5ff62af772cb5c887ca26fa100c
Wklej <kontogje@gmail.com>
2022-03-19 16:26:04 +0100

    + navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+
+        <div className="navbar fixed-top">
+            <p className="col-7 d-flex text-left ms-4">sunnyside</p>
+                <p className="col">About</p>
+                <p className="col">Services</p>
+                <p className="col">Projects</p>
+            <button className="btn btn-outline-primary">Contact</button>
+        </div>
+
+     );
+}
+ 
+export default Navbar;
