ff4d3ec8048bd6ea47c253ffab9b87ad5cad9132
Wklej <kontogje@gmail.com>
2022-04-08 18:40:48 +0200

    + href

--- src/Components/TextSlide.js
+++ src/Components/TextSlide.js
@@ -3,6 +3,7 @@ const TextSlide = ({title, content}) => {
         <div className="col-6">
             <p>{title}</p>
             <p className="mt-2">{content}</p>
+            <a href="#">Learn more</a>
         </div>
     );
 }
