ee5c97723fec1d79a0f26ef3090075490e50bca3
Wklej <kontogje@gmail.com>
2022-04-26 19:42:52 +0200

    styled imageWithText

--- src/Components/ImageWithText.js
+++ src/Components/ImageWithText.js
@@ -1,10 +1,20 @@
 const ImageWithText = ({img, title, content}) => {
+
+    const colorPhoto = {
+        color: 'hsl(198, 62%, 26%)'
+    };
+
+    const colorGraphic = {
+        color: 'hsl(167, 40%, 24%'
+    };
+
     return ( 
-        <div className="col-6 posR">
+        <div className="col-6 posR text-center">
             <img src={img} className="img-fluid" />
-            <div className="posA">
-                <p className="posR">{title}</p>
-                <p className="posR">{content}</p>
+            <div className={"posA imageText"} 
+                style={title === 'Photography' ? colorPhoto : colorGraphic }>
+                <h1 className="posR">{title}</h1>
+                <p className="posR pt-3">{content}</p>
             </div>
         </div>
      );
